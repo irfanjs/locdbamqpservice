302733eea39f1635179cce455ec7646716f06291
Sayed <Irfan.Sayed@Honeywell.com>
2019-07-29 14:52:39 +0530

    first commit

---
+++ src/main/java/com/infy/ci/locdbamqpservice/LocdbamqpserviceApplication.java
@@ -0,0 +1,13 @@
+package com.infy.ci.locdbamqpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocdbamqpserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocdbamqpserviceApplication.class, args);
+	}
+
+}
